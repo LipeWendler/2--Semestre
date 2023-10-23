7c32f40840887a0fc52cbc32b7326c41a3cf74ec
LipeWendler <felipewendlerccbeu@gmail.com>
2023-10-22 21:46:24 -0300

    a

--- 2° Biemstre/Atividade_05/Exercicio_03-04-05-06/Moto.py
+++ 2° Biemstre/Atividade_05/Exercicio_03-04-05-06/Moto.py
@@ -1,14 +1,17 @@
 from Veiculos import Veiculos
 
+#Exercicio 03 --------------------------------------------
+
 class Moto(Veiculos):
     def __init__(self, modelo, marca):
         super().__init__(modelo, tipo="Moto")
         self._marca = marca
 
-    def ligar(self):
-        print(f'{self._modelo} esta ligado!')
+#Exercicio 04 --------------------------------------------
 
     def Descrever(self):
+        print(40*"=")
         print(f'Tipo: {self._tipo}')
         print(f'Modelo: {self._modelo}')
         print(f'Marca: {self._marca}')
+        print(40*"=")
--- 2° Biemstre/Atividade_05/Exercicio_03-04-05-06/Veiculos.py
+++ 2° Biemstre/Atividade_05/Exercicio_03-04-05-06/Veiculos.py
@@ -1,4 +1,5 @@
 '''
+EXERCICIO 03
 Herança de Veículos:
 Crie uma classe Veiculo com atributos como 
 tipo e velocidade e, em seguida, crie classes 
@@ -6,6 +7,7 @@ Carro e Moto que herdem de Veiculo.
 '''
 
 '''
+EXERCICIO 04
 Sobrescrevendo Métodos:
 Na classe Veiculo, crie um método chamado descricao() 
 que imprima uma descrição básica do veículo. Sobrescreva 
@@ -14,18 +16,29 @@ specíficas para cada tipo de veículo.
 '''
 
 '''
+EXERCICIO 05
 Polimorfismo com Métodos:
 Crie uma função chamada acelerar(veiculo) que aceita qualquer
 objeto Veiculo e aumenta sua velocidade. Teste essa função com 
 instâncias de Carro e Moto.
 '''
 
+'''
+EXERCICIO 06
+Polimorfismo com Listas:
+Crie uma lista de objetos Veiculo contendo instâncias de Carro e Moto. 
+Itere sobre a lista e chame o método descricao() para cada objeto.
+'''
+
+
+#Exercicio 03 --------------------------------------------
 
 class Veiculos:
-    def __init__(self, modelo, tipo, funcionando = False):
+    def __init__(self, modelo, tipo, funcionando = False, velocidade = 0):
         self._modelo = modelo
         self._tipo = tipo
         self._funcionando = funcionando
+        self._velocidade = velocidade
 
     @property
     def modelo(self):
@@ -51,19 +64,36 @@ class Veiculos:
     def funcionando(self, funcionando):
         self._funcionando = funcionando
 
+    @property
+    def velocidade(self):
+        return self._velocidade
+    
+    @velocidade.setter
+    def velocidade(self, velocidade):
+        self._velocidade = velocidade
+
 
     def ligar(self):
         print(f'O {self._modelo} está ligado!')
+        self._funcionando = True
+
+#Exercício 04 --------------------------------------------
 
     def descrever(self):
+        print(40*"=")
         print(f'Tipo: {self._tipo}')
         print(f'Modelo: {self._modelo}')
+        print(40*"=")
 
-    def acelerar(self, velocidade):
+#Exercicio 05 --------------------------------------------
+
+    def acelerar(self):
         if not self._funcionando:
             print(f'{self.modelo} não está ligado!')
+            return
+        self._velocidade = self._velocidade + 10
+        print(f'{self._modelo} acelerou! e está a {self._velocidade}Km/h!')
 
-        print(f'{self._modelo} está acelerando a {velocidade}Km/h!')
 
 
         
